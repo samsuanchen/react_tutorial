4b389b110db15834f7144af2bf112ccc1657896e
sam <samsuanchen@gmail.com>
2013-12-23 09:22:08 +0800

    reactive state

--- tutorial.js
+++ tutorial.js
@@ -39,11 +39,14 @@ var CommentList = React.createClass({
   }
 });
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
